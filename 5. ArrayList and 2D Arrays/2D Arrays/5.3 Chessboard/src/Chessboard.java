d8320751ab683ff33938bd34795d7f3cd03c781c
alvar <alvarorgf16@gmail.com>
2023-09-24 19:59:14 +0100

    fourth commit

--- 5. ArrayList and 2D Arrays/2D Arrays/5.3 Chessboard/src/Chessboard.java
+++ 5. ArrayList and 2D Arrays/2D Arrays/5.3 Chessboard/src/Chessboard.java
@@ -4,6 +4,20 @@ public class Chessboard
    {
       char[][] chessboard = new char[8][8];
 
-      /* Create and print a chessboard. */
+      int size = 8;
+      for (int row = 0; row < size; row++){
+         for (int col = 0; col < size; col++) {
+            if ((col + row) % 2 == 0) {
+               chessboard[row][col] = '\u25A1';
+               System.out.print(chessboard[row][col]);
+            }
+            else {
+               chessboard[row][col] = '\u25A0';
+               System.out.print(chessboard[row][col]);
+            }
+
+         }
+         System.out.println();
+      }
    }
 }
--- 5. ArrayList and 2D Arrays/ArrayList/5.2 Average Calculator/src/AverageCalculator.java
+++ 5. ArrayList and 2D Arrays/ArrayList/5.2 Average Calculator/src/AverageCalculator.java
@@ -4,6 +4,11 @@ public class AverageCalculator
 {
     public static double getAverageOf (ArrayList<Integer> numbers)
     {
-        /* TODO */
+        int length = numbers.size();
+        double sum = 0;
+        for (int num : numbers) {
+            sum += num;
+        }
+        return sum / length;
     }
 }
